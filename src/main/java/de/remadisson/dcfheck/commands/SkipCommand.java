12a6ef5845943d0ce98f80ceb21dab15cefb6773
Maximilian Loers <maximilianloers@icloud.com>
2022-10-19 12:45:46 +0200

    Skip can be done by anyone.

--- src/main/java/de/remadisson/dcfheck/commands/SkipCommand.java
+++ src/main/java/de/remadisson/dcfheck/commands/SkipCommand.java
@@ -5,11 +5,9 @@ import de.remadisson.dcfheck.Main;
 import de.remadisson.dcfheck.lavaplayer.PlayerManager;
 import de.remadisson.dcfheck.manager.CommandExecutor;
 import net.dv8tion.jda.api.entities.Channel;
-import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.managers.AudioManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,23 +45,19 @@ public class SkipCommand implements CommandExecutor {
             return;
         }
 
-        if(!event.getGuild().getSelfMember().getVoiceState().inAudioChannel()){
+       /* if(!event.getGuild().getSelfMember().getVoiceState().inAudioChannel()){
             final AudioManager audioManager = event.getGuild().getAudioManager();
             final VoiceChannel vc = (VoiceChannel) event.getMember().getVoiceState().getChannel();
             audioManager.openAudioConnection(vc);
-        }
+        }*/
 
         if(!Objects.equals(event.getMember().getVoiceState().getChannel(), event.getGuild().getSelfMember().getVoiceState().getChannel()) && !event.getUser().getId().equals("268362677313601536")) return;
 
         if(event.getOption("songindex") != null) {
             int songIndex = Integer.parseInt(event.getOption("songindex").getAsString());
-            if(songIndex <= 0){
-                event.reply("Aufgrund des Mappings, kann der 0te Song nicht gefunden werden, da er sonst der -1te Song wäre. Pech gehabt. Versuch es doch einfach nochmal.").queue(msg -> {
-                    msg.deleteOriginal().queueAfter(20, TimeUnit.SECONDS);
-                });
-                return;
-            }
-            AudioTrackInfo audioTrackInfo = PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).scheduler.skipTopSongIndex(songIndex);
+            if(songIndex <= 0) songIndex = 1;
+
+            AudioTrackInfo audioTrackInfo = PlayerManager.getINSTANCE().getMusicManager().scheduler.skipToSongIndex(songIndex);
             event.reply("Jetzt spielt: `" + audioTrackInfo.title + "` von `" + audioTrackInfo.author + "`").queue();
         } else {
             PlayerManager.getINSTANCE().nextTrack(event);
